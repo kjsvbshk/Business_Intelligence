377e7388103384b5078f5148c87e8a02fd8fe393
kjsvbshk <irvingrios20023@gmail.com>
2025-03-08 21:21:13 -0500

    new exportable datas

--- Datos_Exportables.sql
+++ Datos_Exportables.sql
@@ -385,7 +385,7 @@ CREATE TABLE `Flujo_Caja` (
   CONSTRAINT `Flujo_Caja_ibfk_4` FOREIGN KEY (`Compra_ID`) REFERENCES `Compras` (`ID`) ON DELETE SET NULL,
   CONSTRAINT `Flujo_Caja_ibfk_5` FOREIGN KEY (`Gastos_Nomina_ID`) REFERENCES `Gastos_Nomina` (`ID`) ON DELETE SET NULL,
   CONSTRAINT `Flujo_Caja_ibfk_6` FOREIGN KEY (`Impuesto_ID`) REFERENCES `Impuestos` (`ID`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=379 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=366 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -736,4 +736,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-04  4:06:27
+-- Dump completed on 2025-03-09  2:20:25
